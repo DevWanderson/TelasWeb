a35eee20aeb996901db2935d5ad76b458d0597ad
WANDERSON GUEDES LOPES <guedesw3@gmail.com>
2022-02-24 00:37:59 -0300

    atualização Readme

--- javascript/scriptLogin.js
+++ javascript/scriptLogin.js
@@ -1,4 +1,15 @@
-function mudaFoto(foto) {
+let inputs = document.getElementsByClassName('input-form');
+		for (let input of inputs) {
+			input.addEventListener("blur", function() {
+				if(input.value.trim() != ""){
+					input.classList.add("has-val");
+				} else {
+					input.classList.remove("has-val");
+				}
+			});
+		}
+		
+/*function mudaFoto(foto) {
 	document.getElementById("icone").src = foto;
 }
 
@@ -26,3 +37,4 @@ function quebraLampada(){
 }
 
 <img src="_imagens/lampada-apagada.jpg" id="luz" onmousemove="acendeLampada()" onmouseout="apagaLampada()" onclick="quebraLampada()"/>
+*/
